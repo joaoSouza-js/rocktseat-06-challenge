fad73cc6bb73c0ca786958c2d3d2acf55330d071
joao victor <jvictor@a5solutions.com>
2026-07-07 20:57:58 -0300

    add prisma and environment  in app module

--- src/infra/services/prisma.service.ts
+++ src/infra/services/prisma.service.ts
@@ -1,6 +1,33 @@
-import { Injectable } from "@nestjs/common";
+import { PrismaClient } from '@/generated/prisma/client';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { ApiConfigService } from './api-config.service';
+
 
 @Injectable()
-export class PrismaService {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+
+    constructor(apiConfigService: ApiConfigService) {
+
+        const adapter = new PrismaPg({
+            connectionString: apiConfigService.databaseUrl,
+
+
+        }, {
+            schema: apiConfigService.schemaId
+        })
+        super({
+            adapter,
+            log: ["error", "warn"],
+        })
+    }
+    onModuleInit() {
+        return this.$connect()
+    }
+    onModuleDestroy() {
+        return this.$disconnect()
+    }
+
+
 
 }
